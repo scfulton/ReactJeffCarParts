673d274ca45d9df2c8806daed6993a2586dd3f79
Scotty Fulton <sfulton@talentpath.com>
2019-10-24 14:40:11 -0500

    removed <a> tag too, now a blank black screen

--- jeff-car-parts/src/App.js
+++ jeff-car-parts/src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">
+
       </header>
     </div>
   );
